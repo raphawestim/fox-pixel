0a2b1a76a7de7d8426a7ecea396b16a760275016
Raphael <raphawestim45@gmail.com>
2023-09-28 09:59:26 -0300

    v.1.0.1

--- js/slider.js
+++ js/slider.js
@@ -1,10 +1,10 @@
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
   spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
